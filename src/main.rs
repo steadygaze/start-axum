915c006eb0e79c0eb9d3e9e57bd80292efe03342
Greg Johnston <greg.johnston@gmail.com>
2023-01-09 18:55:54 -0500

    Update starter for new `cargo leptos` integrations

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,27 @@ use cfg_if::cfg_if;
 cfg_if! {
     if #[cfg(feature = "ssr")] {
         use leptos::*;
-        use actix_files::{Files, NamedFile};
-        use actix_web::{HttpServer, middleware::Compress, web};
         use leptos_start::app::*;
+        use actix_files::{Files};
+        use actix_web::*;
+        use leptos_actix::{LeptosRoutes, generate_route_list};
 
         #[actix_web::main]
         async fn main() -> std::io::Result<()> {
             let conf = get_configuration(Some("Cargo.toml")).await.unwrap();
-            let addr = conf.leptos_options.site_address.clone();
-
-            simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
-            log::info!("serving at http://{addr}");
+            let addr = conf.leptos_options.site_address;
+            // Generate the list of routes in your Leptos App
+            let routes = generate_route_list(|cx| view! { cx, <App/> });
 
             HttpServer::new(move || {
                 let leptos_options = &conf.leptos_options;
                 let site_root = &leptos_options.site_root;
-                let pkg_dir = &leptos_options.site_pkg_dir;
-                let bundle_path = format!("/{site_root}/{pkg_dir}");
-                let output_name = leptos_options.output_name.clone();
 
-                actix_web::App::new()
-                    // used by cargo-leptos. Can be removed if using wasm-pack and cargo run.
-                    .service(Files::new(&bundle_path, format!("./{bundle_path}")))
-                    .route("/style.css", web::get().to(move || {
-                        let bundle_path = bundle_path.clone();
-                        let output_name = output_name.clone();
-                        async move {
-                            NamedFile::open_async(format!("./{}/{}.css", bundle_path, output_name)).await.expect("could not open CSS file")
-                        }
-                    }))
+                App::new()
                     .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
-                    .route("/{tail:.*}", leptos_actix::render_app_to_stream(leptos_options.to_owned(), |cx| view! { cx, <App/> }))
-                    .wrap(Compress::default())
+                    .leptos_routes(leptos_options.to_owned(), routes.to_owned(), |cx| view! { cx, <App/> })
+                    .service(Files::new("/", site_root))
+                //.wrap(middleware::Compress::default())
             })
             .bind(&addr)?
             .run()
