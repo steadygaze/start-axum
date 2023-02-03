4c4fab3c6e32fca993d380254a2e488b91588ac1
Indrazar <110272232+Indrazar@users.noreply.github.com>
2023-02-03 13:00:36 -0500

    update get_configuration comment for clarity

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ async fn main() {
 
     simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos
+    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
+    // For deployment these variables are:
+    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
+    // Alternately a file can be specified such as Some("Cargo.toml")
+    // The file would need to be included with the executable when moved to deployment
     let conf = get_configuration(None).await.unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_address;
