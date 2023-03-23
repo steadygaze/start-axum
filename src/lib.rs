f96d7b4da111f8e0bd7a0b791fc21e64c77e9371
Indrazar <110272232+Indrazar@users.noreply.github.com>
2023-03-23 10:50:51 -0400

    chore: update deps, cleanup warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use cfg_if::cfg_if;
-use leptos::*;
 pub mod app;
 pub mod error_template;
 pub mod fileserv;
 
 cfg_if! { if #[cfg(feature = "hydrate")] {
-
+    use leptos::*;
     use wasm_bindgen::prelude::wasm_bindgen;
     use crate::app::*;
 
