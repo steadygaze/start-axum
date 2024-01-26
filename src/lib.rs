a5cdab4a4071bf9f2d3884d87e70cc172d23f4ba
Greg Johnston <greg.johnston@gmail.com>
2024-01-26 16:51:40 -0500

    remove redundant import

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,6 @@ pub mod fileserv;
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     use crate::app::*;
-    use leptos::*;
     console_error_panic_hook::set_once();
-
     leptos::mount_to_body(App);
 }
